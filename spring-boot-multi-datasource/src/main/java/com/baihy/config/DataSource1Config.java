1cf9be099469341f2067911916fb708042cddacc
huayang.bai <1053091115@qq.com>
2019-05-18 18:28:44 +0800

    实现了分包数据源

--- spring-boot-multi-datasource/src/main/java/com/baihy/config/DataSource1Config.java
+++ spring-boot-multi-datasource/src/main/java/com/baihy/config/DataSource1Config.java
@@ -22,19 +22,16 @@ import javax.sql.DataSource;
 @Configuration
 @MapperScan(basePackages = {"com.baihy.datasource1.mapper"}, sqlSessionFactoryRef = "test1SqlSessionFactory")
 public class DataSource1Config {
-
     //1.配置数据源
     //2.配置SqlSessionFactory
     //3.配置事务管理器
     //4.配置template
-
     // 配置数据源
     @Bean(name = "test1DataSource")
     @ConfigurationProperties(prefix = "spring.datasource.test1")
     public DataSource dataSource() {
         return DataSourceBuilder.create().build();
     }
-
     // 配置SqlSessionFactory
     @Bean(name = "test1SqlSessionFactory")
     public SqlSessionFactory sqlSessionFactory(DataSource test1DataSource) throws Exception {
@@ -42,19 +39,14 @@ public class DataSource1Config {
         sqlSessionFactoryBean.setDataSource(test1DataSource);
         return sqlSessionFactoryBean.getObject();
     }
-
-
     // 配置事务管理器
     @Bean(name = "test1DataSourceTransactionManager")
     public DataSourceTransactionManager dataSourceTransactionManager(DataSource test1DataSource) {
         return new DataSourceTransactionManager(test1DataSource);
     }
-
-
     // 配置template
     @Bean(name = "test1SqlSessionTemplate")
     public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory test1SqlSessionFactory) {
         return new SqlSessionTemplate(test1SqlSessionFactory);
     }
-
 }
