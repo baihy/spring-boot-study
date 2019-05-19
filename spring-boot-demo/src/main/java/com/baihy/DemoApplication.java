dc4ed91f4b60e7ee0f95818c2c989da4fb356204
huayang.bai <1053091115@qq.com>
2019-05-19 12:02:41 +0800

    添加了pagehelper的支持

--- spring-boot-demo/src/main/java/com/baihy/DemoApplication.java
+++ spring-boot-demo/src/main/java/com/baihy/DemoApplication.java
@@ -2,8 +2,8 @@ package com.baihy;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @projectName: springboot
@@ -12,9 +12,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * @author: huayang.bai
  * @date: 2019-05-12 11:18
  */
-@SpringBootApplication
+// @SpringBootApplication //原理是扫描同级包
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.baihy.controller", "com.baihy.service"}) // 这里是定位扫描包
 @MapperScan(basePackages = {"com.baihy.mapper"})
-@EnableTransactionManagement
 public class DemoApplication {
 
     public static void main(String[] args) {
